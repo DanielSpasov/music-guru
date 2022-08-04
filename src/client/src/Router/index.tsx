44b562c0a2a6f4f62bca50aff976ea50c9a2f1e5
Daniel Spasov <danielspasov4@gmail.com>
2022-08-04 07:45:50 +0300

    Changed router directory

--- src/client/src/Router/index.tsx
+++ src/client/src/Router/index.tsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import routes from './Config/routes.json';
-import { IRoute } from './Interfaces';
-import pages from './Pages';
+import { IRoute } from '../Interfaces';
+import routes from './routes.json';
+import pages from '../Pages';
 
 const transformRoute = (route: any): IRoute => {
   return {
     component: route?.component || 'Loader',
-    isPrivate: route?.isPrivate || false,
+    isPrivate: route.hasOwnProperty('isPrivate') ? route.isPrivate : false,
     path: route?.path || '/home',
     children: route?.children || null
   };
 };
 
 const renderChildren = (routes: IRoute[]): JSX.Element[] => {
-  return routes.map((x: IRoute) => setupRoute(x));
+  return routes.map(transformRoute).map((x: IRoute) => setupRoute(x));
 };
 
 const setupRoute = (route: IRoute): JSX.Element => {
@@ -36,12 +36,10 @@ const setupRoute = (route: IRoute): JSX.Element => {
 
 export default function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={pages.Home()} />
-        {routes.map(transformRoute).map((route: IRoute) => setupRoute(route))}
-        <Route path="*" element={pages.NotHome()} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route index element={pages.Home()} />
+      {routes.map(transformRoute).map((route: IRoute) => setupRoute(route))}
+      <Route path="*" element={pages.NotHome()} />
+    </Routes>
   );
 }
