51a0ef9ef9dc2bf7cb91cfb51eafa53e1fc22b08
Daniel Spasov <shadygotrabies@gmail.com>
2023-02-13 09:15:48 +0200

    Added search funcitonality in the backend

--- src/server/src/Services/Artist/fetch.ts
+++ src/server/src/Services/Artist/fetch.ts
@@ -6,7 +6,11 @@ import { transformArtist } from '../../Transforms';
 
 export async function fetch(req: Request, res: Response) {
   try {
-    const artists = await ArtistModel.find()
+    const artists = await ArtistModel.find(
+      req.query.search
+        ? { name: { $regex: req.query.search, $options: 'i' } }
+        : {}
+    )
       .limit(25)
       .transform(x => x.map(transformArtist));
     res.status(200).json({ data: artists });
