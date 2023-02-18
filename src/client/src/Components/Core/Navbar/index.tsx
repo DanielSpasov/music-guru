c9ed66e358b19e57478678ea00e864b60dbda7b2
Daniel Spasov <shadygotrabies@gmail.com>
2023-02-18 09:58:58 +0200

    Allowed global searching for singles

--- src/client/src/Components/Core/Navbar/index.tsx
+++ src/client/src/Components/Core/Navbar/index.tsx
@@ -63,7 +63,7 @@ export default function Navbar() {
         </Link>
       </Box>
       <Box display="flex" alignContent="center" height="60px">
-        <Search models={['artists']} />
+        <Search models={['artists', 'singles']} />
         <Dropdown label="Settings" icon={{ model: 'gear', type: 'solid' }}>
           <Link to="/theme">Theme</Link>
         </Dropdown>
