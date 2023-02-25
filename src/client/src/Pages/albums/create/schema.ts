b236050ac52b8b317702996a93498b4d6120430d
Daniel Spasov <shadygotrabies@gmail.com>
2023-02-25 13:57:32 +0200

    Added album creation

---
+++ src/client/src/Pages/albums/create/schema.ts
@@ -0,0 +1,29 @@
+import { FormSchema } from '../../../Components/Forms/Form/helpers';
+import { Input, Select } from '../../../Components';
+import Api from '../../../Api';
+
+export const schema: FormSchema = {
+  fields: [
+    {
+      key: 'name',
+      type: 'text',
+      label: 'Name',
+      required: true,
+      Component: Input
+    },
+    {
+      key: 'image',
+      type: 'text',
+      label: 'Image URL',
+      required: true,
+      Component: Input
+    },
+    {
+      key: 'artist',
+      label: 'Artist',
+      fetchFn: ({ params }: any) => Api.artists.fetch({ config: { params } }),
+      Component: Select,
+      required: true
+    }
+  ]
+};
--- src/server/src/Controllers/Models/Album.ts
+++ src/server/src/Controllers/Models/Album.ts
@@ -1,10 +1,33 @@
 import { Router } from 'express';
 
-import { AlbumModel, IAlbum } from '../../Database/Schemas';
-import { fetch } from '../../Services/requests';
+import { AlbumModel, ArtistModel, IAlbum } from '../../Database/Schemas';
+import { AlbumSchema } from '../../Types/Album';
+import { fetch, post } from '../../Services/requests';
+import { CustomError } from '../../Error/CustomError';
 
 const router = Router();
 
 router.get('/', (req, res) => fetch<IAlbum>({ req, res, Model: AlbumModel }));
+
+router.post('/', (req, res) =>
+  post<IAlbum>({
+    req,
+    res,
+    Model: AlbumModel,
+    ValidationSchema: AlbumSchema,
+    preCreateFn: async (data: any) => {
+      const artist = await ArtistModel.findOne({ uid: data.artist });
+      if (!artist) {
+        throw new CustomError({ message: 'Artist not found.', code: 404 });
+      }
+
+      return {
+        data: {
+          artist: artist._id
+        }
+      };
+    }
+  })
+);
 
 export default router;
