994f2bf2400361e18b1ca22febdd4300950cbcd8
Daniel Spasov <danielspasov4@gmail.com>
2023-04-15 10:11:56 +0300

    First Successful email sent

--- src/server/src/Controllers/Models/User.ts
+++ src/server/src/Controllers/Models/User.ts
@@ -1,8 +1,32 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import nodemailer from 'nodemailer';
 
 import { SignUp, SignIn, ValidateToken } from '../../Services/Auth';
 
 const router = Router();
+
+router.get('/test-nodemailer', async (req: Request, res: Response) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_SERVICE_USER,
+      pass: process.env.EMAIL_SERVICE_PASS
+    }
+  });
+
+  const info = await transporter.sendMail({
+    from: `"Music Nerd Accounts" <${process.env.EMAIL_SERVICE_USER}>`,
+    to: 'shadygotrabies@gmail.com',
+    subject: 'Test Subject',
+    text: 'Test Text',
+    html: '<b>Test HTML</b>'
+  });
+
+  res.status(200).json({
+    message: 'Successful',
+    info
+  });
+});
 
 router.get('/validate-jwt', ValidateToken);
 
