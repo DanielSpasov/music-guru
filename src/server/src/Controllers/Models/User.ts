06828cbc7e4bb71005b62b5fc9fa0efb8211ee00
Daniel Spasov <shadygotrabies@gmail.com>
2023-06-10 10:11:46 +0300

    Added ability to send emails

--- src/server/src/Controllers/Models/User.ts
+++ src/server/src/Controllers/Models/User.ts
@@ -1,30 +1,8 @@
-import { Request, Response, Router } from 'express';
-import nodemailer from 'nodemailer';
+import { Router } from 'express';
 
 import { SignUp, SignIn, ValidateToken } from '../../Services/Auth';
 
 const router = Router();
-
-router.get('/test-nodemailer', async (req: Request, res: Response) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_SERVICE_USER,
-      pass: process.env.EMAIL_SERVICE_PASS
-    }
-  });
-
-  const code = Math.random().toString().substring(2, 8);
-
-  await transporter.sendMail({
-    from: `"Music Nerd" <${process.env.EMAIL_SERVICE_USER}>`,
-    to: 'shadygotrabies@gmail.com',
-    subject: '2FA Code',
-    text: `2FA Code: ${code}`
-  });
-
-  res.status(200).json({ message: 'Email sent to @email.com' });
-});
 
 router.get('/validate-jwt', ValidateToken);
 
