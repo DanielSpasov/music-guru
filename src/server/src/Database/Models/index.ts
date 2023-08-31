a496d64d33be41acb1d642ca2c43634bf46fbe0f
Daniel Spasov <shadygotrabies@gmail.com>
2023-08-31 13:13:27 +0300

    Updated index imports

--- src/server/src/Database/Models/index.ts
+++ src/server/src/Database/Models/index.ts
@@ -1,4 +1,15 @@
-export { default as UserModel, IUser } from './User';
-export { default as ArtistModel, IArtist } from './Artist';
-export { default as SongModel, ISong } from './Song';
-export { default as AlbumModel, IAlbum } from './Album';
+// User
+export { default as UserModel } from './User';
+export { IUser } from './User/types';
+
+// Aritst
+export { default as ArtistModel } from './Artist';
+export { IArtist } from './Artist/types';
+
+// Song
+export { default as SongModel } from './Song';
+export { ISong } from './Song/types';
+
+// Album
+export { default as AlbumModel } from './Album';
+export { IAlbum } from './Album/types';
