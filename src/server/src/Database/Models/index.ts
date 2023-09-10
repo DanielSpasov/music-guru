e0fd35dc15509c7ebe6f75acdd5266108364fa20
Daniel Spasov <shadygotrabies@gmail.com>
2023-09-10 08:01:51 +0300

    Removed obsolete code & mongoose from package.json

--- src/server/src/Database/Models/index.ts
+++
@@ -1,7 +0,0 @@
-// Song
-export { default as SongModel } from './Song';
-export { ISong } from './Song/types';
-
-// Album
-export { default as AlbumModel } from './Album';
-export { IAlbum } from './Album/types';
