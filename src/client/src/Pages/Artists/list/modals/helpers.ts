ebd21cd72c8aafdee8adf68fb52a5b3ed99c5df3
Daniel Spasov <shadygotrabies@gmail.com>
2023-10-16 09:08:22 +0300

    Fixed types

--- src/client/src/Pages/Artists/list/modals/helpers.ts
+++ src/client/src/Pages/Artists/list/modals/helpers.ts
@@ -3,10 +3,10 @@ import { Dispatch, SetStateAction } from 'react';
 export type ModalsProps = {
   openCreate: boolean;
   setOpenCreate: Dispatch<SetStateAction<boolean>>;
-  fetchArtists: () => void;
+  fetchArtists: () => Promise<void>;
 };
 
 export type CreateArtistProps = {
   onClose: () => void;
-  fetchArtists: () => void;
+  fetchArtists: () => Promise<void>;
 };
