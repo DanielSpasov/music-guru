88236cdc8be7915c4e9321d19e54420f9720dac3
Daniel Spasov <shadygotrabies@gmail.com>
2023-10-18 11:17:41 +0300

    Fixed invalid import path

--- src/client/src/Components/HTML/Icon/SVG.tsx
+++ src/client/src/Components/HTML/Icon/SVG.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { IconProps } from './helpers';
+import { IconProps } from './';
 
 export const SVG = styled('svg')<IconProps>`
   xmlns: 'http://www.w3.org/2000/svg';
