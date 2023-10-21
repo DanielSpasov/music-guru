0801a71acde23ebaaa5c59babc7a39bb5b614ca9
Daniel Spasov <shadygotrabies@gmail.com>
2023-10-21 13:06:05 +0300

    Update Skeletons Updated Song Card

--- src/client/src/Components/Common/Card/Song.tsx
+++ src/client/src/Components/Common/Card/Song.tsx
@@ -4,17 +4,17 @@ import { CardProps } from './helpers';
 export default function SongCard({ data, onClick }: CardProps<Song>) {
   return (
     <div
-      className="flex items-center w-48 h-16 p-2 m-2 rounded-md hover:bg-neutral-700 cursor-pointer"
+      className="flex items-center w-52 h-20 p-2 m-2 rounded-md hover:bg-neutral-700 cursor-pointer"
       onClick={onClick}
     >
       <img
         onClick={onClick}
         src={data?.image || '/images/logo/blue-logo-square512.png'}
-        className="w-12 h-12 rounded-sm"
+        className="w-16 h-16 rounded-sm"
       />
 
-      <div className="h-full px-2">
-        <span className="whitespace-normal">{data.name}</span>
+      <div className="h-20 px-2 py-1 whitespace-normal overflow-hidden">
+        <span>{data.name}</span>
       </div>
     </div>
   );
--- src/client/src/Components/Common/List/skeleton.tsx
+++ src/client/src/Components/Common/List/skeleton.tsx
@@ -1,60 +1,25 @@
-import styled, { keyframes } from 'styled-components';
-
-import { Theme } from '../../Core/ThemeSwitcher/helpers';
 import { ModelKeys } from '../../../Api/helpers';
 import { SkeletonProps } from './helpers';
-import { Box } from '../../HTML';
-
-const Animation = keyframes`
-  0%,
-  100% {
-    background-color: #404040;
-  }
-  50% {
-    background-color: #3a3a3a;
-  }
-`;
-
-const AnimatedBox = styled(Box)<{ theme: Theme }>`
-  animation: ${Animation} 1s infinite;
-`;
 
 function CardSkeleton({ model }: { model: ModelKeys }) {
   switch (model) {
     case 'artists':
       return (
-        <Box
-          margin="1em"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-        >
-          <AnimatedBox width="200px" height="200px" borderRadius="50%" />
-          <AnimatedBox width="60px" height="20px" margin=".5em" />
-        </Box>
+        <div className="flex flex-col items-center m-4 animate-pulse">
+          <div className="bg-neutral-800 w-48 h-48 rounded-full" />
+          <div className="bg-neutral-800 w-16 h-6 m-1 rounded-md" />
+        </div>
       );
     case 'songs':
       return (
-        <Box
-          margin=".5em"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-        >
-          <AnimatedBox width="200px" height="50px" />
-        </Box>
+        <div className="w-52 h-20 m-2 bg-neutral-800 rounded-md animate-pulse" />
       );
     default:
       return (
-        <Box
-          margin="1em"
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-        >
-          <AnimatedBox width="200px" height="200px" borderRadius="1em" />
-          <AnimatedBox width="60px" height="20px" margin=".5em" />
-        </Box>
+        <div className="flex flex-col items-center m-4 animate-pulse">
+          <div className="bg-neutral-800 w-48 h-48 rounded-md" />
+          <div className="bg-neutral-800 w-16 h-6 m-1 rounded-md" />
+        </div>
       );
   }
 }
