2c6251150257f06b668c1713bbeba431faa632ec
Daniel Spasov <shadygotrabies@gmail.com>
2023-10-21 12:39:17 +0300

    Fixed Album card

--- src/client/src/Components/Common/Card/Album.tsx
+++ src/client/src/Components/Common/Card/Album.tsx
@@ -3,20 +3,22 @@ import { CardProps } from './helpers';
 
 export default function AlbumCard({ data, onClick }: CardProps<Album>) {
   return (
-    <div className="flex flex-col items-center m-4 hover:text-primary cursor-pointer">
+    <div
+      className="flex flex-col-reverse items-center m-4 cursor-pointer hover:text-primary [&>span]:hover:text-primary"
+      onClick={onClick}
+    >
+      <span className="text-lg p-2 text-center truncate w-48 [&+div>img]:hover:-top-2">
+        {data.name}
+      </span>
+
       <div className="relative h-48 w-48">
         <div className="absolute w-48 h-48 top-0 rounded-md bg-primary" />
         <img
           alt={data.name}
           src={data.image}
-          onClick={onClick}
           className="absolute w-48 h-48 top-0 rounded-md hover:-top-2"
         />
       </div>
-
-      <span className="text-lg p-2 text-center truncate w-48" onClick={onClick}>
-        {data.name}
-      </span>
     </div>
   );
 }
