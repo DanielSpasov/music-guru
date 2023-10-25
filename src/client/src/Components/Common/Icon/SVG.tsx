d5d625d0cfa1aefde38dca1f64c324aabe799870
Daniel Spasov <shadygotrabies@gmail.com>
2023-10-25 08:42:51 +0300

    Updated SVG Props

--- src/client/src/Components/Common/Icon/SVG.tsx
+++ src/client/src/Components/Common/Icon/SVG.tsx
@@ -2,19 +2,26 @@ import { SVGProps } from './';
 
 export default function SVG({
   disabled = false,
-  className = 'h-9 w-9',
+  className,
   children,
   viewBox,
   onClick
 }: SVGProps) {
+  const defaultProps = '[&>path]:text-primary [&>path]:hover:text-secondary';
+  const darkProps =
+    'dark:[&>path]:hover:text-primary-dark dark:[&>path]:text-neutral-50';
+
+  const onClickProps = onClick && `cursor-pointer ${darkProps} ${defaultProps}`;
+  const disabledProps = disabled
+    ? `[&>path]:text-neutral-300 dark:[&>path]:text-neutral-500`
+    : onClickProps;
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox={viewBox}
       onClick={!disabled && onClick ? onClick : () => null}
-      className={`${
-        disabled ? '[&>path]:text-neutral-500' : onClick && 'cursor-pointer'
-      } ${className}`}
+      className={`${disabledProps} ${className}`}
     >
       {children}
     </svg>
