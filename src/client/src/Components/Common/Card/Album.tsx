6ddae3c1d4850ebca71eb508077c8d7837fd7495
Daniel Spasov <shadygotrabies@gmail.com>
2023-10-25 10:18:38 +0300

    Album card change

--- src/client/src/Components/Common/Card/Album.tsx
+++ src/client/src/Components/Common/Card/Album.tsx
@@ -1,27 +1,25 @@
 import { Album } from '../../../Pages/albums/helpers';
 import { CardProps } from './helpers';
 
-const darkProps =
-  'dark:hover:text-primary-dark dark:[&>span]:hover:text-primary-dark';
+const hoverProps = '[&>span]:hover:text-primary [&>div>img]:hover:-top-2';
+const darkProps = 'dark:[&>span]:hover:text-primary-dark';
 
 export default function AlbumCard({ data, onClick }: CardProps<Album>) {
   return (
     <div
-      className={`flex flex-col-reverse items-center m-4 cursor-pointer hover:text-primary [&>span]:hover:text-primary ${darkProps}`}
+      className={`flex flex-col items-center m-4 cursor-pointer bg-neutral-900 shadow-md rounded-md ${hoverProps} ${darkProps}`}
       onClick={onClick}
     >
-      <span className="text-lg p-2 text-center truncate w-48 [&+div>img]:hover:-top-2">
-        {data.name}
-      </span>
-
       <div className="relative h-48 w-48">
         <div className="absolute w-48 h-48 top-0 rounded-md bg-primary dark:bg-primary-dark" />
         <img
           alt={data.name}
           src={data.image}
-          className="absolute w-48 h-48 top-0 rounded-md hover:-top-2"
+          className="absolute w-48 h-48 top-0 rounded-md"
         />
       </div>
+
+      <span className="text-lg p-2 text-center truncate w-48">{data.name}</span>
     </div>
   );
 }
