360d870f420715a4b6d55552e8555bc4394fd090
Daniel Spasov <shadygotrabies@gmail.com>
2023-10-26 12:41:06 +0300

    Updated Album skeleton to light mode

--- src/client/src/Components/Common/Card/Album.tsx
+++ src/client/src/Components/Common/Card/Album.tsx
@@ -1,8 +1,11 @@
+import moment from 'moment';
 import { Album } from '../../../Pages/albums/helpers';
 import { CardProps } from './helpers';
 
-const hoverProps = '[&>span]:hover:text-primary [&>div>img]:hover:-top-2';
-const darkProps = 'dark:[&>span]:hover:text-primary-dark';
+const hoverProps =
+  '[&>img]:hover:-top-7 [&>div:nth-child(2)>span:nth-child(1)]:hover:text-primary';
+const darkProps =
+  'dark:bg-neutral-900 dark:[&>div:nth-child(2)>span:nth-child(1)]:hover:text-primary-dark';
 
 export default function AlbumCard({
   data,
@@ -12,28 +15,35 @@ export default function AlbumCard({
   if (loading) return <Skeleton />;
   return (
     <div
-      className={`flex flex-col items-center m-4 cursor-pointer bg-neutral-900 shadow-md rounded-md ${hoverProps} ${darkProps}`}
+      className={`relative flex flex-col items-center bg-neutral-200 rounded-md m-3 cursor-pointer shadow-md ${hoverProps} ${darkProps}`}
       onClick={onClick}
     >
-      <div className="relative h-48 w-48">
-        <div className="absolute w-48 h-48 top-0 rounded-md bg-primary dark:bg-primary-dark" />
+      <div className="w-44 h-44 p-2">
         <img
           alt={data.name}
           src={data.image}
-          className="absolute w-48 h-48 top-0 rounded-md"
+          className="w-full h-full rounded-md"
         />
       </div>
 
-      <span className="text-lg p-2 text-center truncate w-48">{data.name}</span>
+      <div className="flex flex-col pb-2">
+        <span className="text-md truncate w-44 px-2">{data.name}</span>
+        <span className="text-md text-neutral-500 truncate w-44 px-2">
+          {moment(data.release_date).year()} • Album
+        </span>
+      </div>
     </div>
   );
 }
 
 function Skeleton() {
   return (
-    <div className="flex flex-col items-center m-4 animate-pulse">
-      <div className="bg-neutral-900 w-48 h-48 rounded-md" />
-      <div className="bg-neutral-900 w-16 h-6 m-1 rounded-md" />
+    <div className="flex flex-col items-center m-3 animate-pulse bg-neutral-200 dark:bg-neutral-900 rounded-md">
+      <div className="bg-neutral-300 dark:bg-neutral-700 w-40 h-40 m-2 rounded-md" />
+      <div className="bg-neutral-200 dark:bg-neutral-900 w-full h-14 pb-1 rounded-md">
+        <div className="bg-neutral-300 dark:bg-neutral-700 rounded-md w-24 h-5 mx-2 mb-2" />
+        <div className="bg-neutral-300 dark:bg-neutral-700 rounded-md w-16 h-5 mx-2 mb-2" />
+      </div>
     </div>
   );
 }
