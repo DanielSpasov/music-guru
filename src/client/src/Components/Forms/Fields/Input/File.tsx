6344f20444db3d6e559f1e648f66f62c6f4614a4
Daniel Spasov <shadygotrabies@gmail.com>
2023-10-28 12:40:51 +0300

    Fixed not being able to focus file input

--- src/client/src/Components/Forms/Fields/Input/File.tsx
+++ src/client/src/Components/Forms/Fields/Input/File.tsx
@@ -1,8 +1,10 @@
+import { useRef } from 'react';
 import { FileProps } from './helpers';
 
 const hoverProps = 'hover:border-dashed hover:border-neutral-300';
+const focusProps = 'focus:border-dashed focus:border-primary';
 const darkProps =
-  'dark:bg-neutral-800 dark:border-neutral-600 dark:hover:border-neutral-500';
+  'dark:bg-neutral-800 dark:border-neutral-600 dark:hover:border-neutral-500 dark:focus:border-primary-dark';
 
 export default function File({
   id,
@@ -12,19 +14,27 @@ export default function File({
   accept,
   value
 }: FileProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <>
       <input
         id={id}
         type="file"
         name={name}
+        ref={inputRef}
         className="hidden"
         onChange={_onChange}
         accept={accept.join(',')}
       />
       <label
         htmlFor={id}
-        className={`block bg-neutral-100 border-2 border-neutral-200 w-full h-11 rounded-md cursor-pointer ${darkProps} ${hoverProps}`}
+        tabIndex={0}
+        onKeyDown={e => {
+          if (e.key !== 'Enter') return;
+          inputRef.current?.click();
+        }}
+        className={`block bg-neutral-100 border-2 border-neutral-200 w-full h-11 rounded-md cursor-pointer outline-none ${hoverProps} ${focusProps} ${darkProps}`}
       >
         <span
           className={`absolute ${!value ? 'top-2.5 left-3' : '-top-7 left-1'}`}
