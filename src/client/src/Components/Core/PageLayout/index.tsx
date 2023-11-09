5126427de21c9221e218db709e86730e2fb08c90
Daniel Spasov <shadygotrabies@gmail.com>
2023-11-09 10:01:09 +0200

    Updated Actions

--- src/client/src/Components/Core/PageLayout/index.tsx
+++ src/client/src/Components/Core/PageLayout/index.tsx
@@ -45,20 +45,25 @@ export default function PageLayout({
   );
 }
 
+const darkActionProps =
+  'dark:hover:shadow-lg dark:hover:shadow-neutral-950 dark:shadow-neutral-950 dark:bg-primary-dark';
+
 function Action({ action }: { action: IAction }) {
   const [showTooltip, setShowTooltip] = useState(false);
 
   return (
-    <div
-      className="flex items-center bg-neutral-300 dark:bg-neutral-900 shadow-lg rounded-full m-4"
-      onMouseLeave={() => setShowTooltip(false)}
-      onMouseEnter={() => setShowTooltip(true)}
-    >
-      <Icon
-        model={action.icon}
-        onClick={action.onClick}
-        className="w-14 h-14 p-3"
-      />
+    <div className="flex items-center m-4">
+      <div
+        className={`rounded-full shadow-md bg-primary hover:opacity-70 ${darkActionProps}`}
+        onMouseLeave={() => setShowTooltip(false)}
+        onMouseEnter={() => setShowTooltip(true)}
+      >
+        <Icon
+          model={action.icon}
+          onClick={action.onClick}
+          className={`w-14 h-14 p-3 [&>path]:fill-white`}
+        />
+      </div>
       {action?.tooltip && (
         <Tooltip text={action?.tooltip} shown={showTooltip} />
       )}
