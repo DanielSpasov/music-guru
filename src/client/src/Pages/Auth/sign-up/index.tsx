d10ca64aff00bf40c8f2c486f8bc247ec2de2e65
Daniel Spasov <shadygotrabies@gmail.com>
2024-02-04 20:47:49 +0200

    Fix Sign-In/Up forms styles

--- src/client/src/Pages/Auth/sign-up/index.tsx
+++ src/client/src/Pages/Auth/sign-up/index.tsx
@@ -28,7 +28,7 @@ export default function SignUp() {
 
   return (
     <PageLayout title="Sign Up" showHeader={false}>
-      <div className="w-1/2 m-auto mt-5">
+      <div className="bg-neutral-900 rounded-lg max-w-2xl min-w-fit w-1/2 m-auto mt-5">
         <Form
           validationSchema={SignUpSchema as any}
           onSubmit={onSubmit}
@@ -45,6 +45,7 @@ export default function SignUp() {
           schema={[
             {
               key: 'fields',
+              title: 'Account Information',
               fields: [
                 {
                   key: 'username',
