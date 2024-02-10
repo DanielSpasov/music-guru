6881d8b20daa0a78b3dfe1457ae471b75ed39643
Daniel Spasov <shadygotrabies@gmail.com>
2024-02-10 10:23:13 +0200

    Added Link/Navlink tests

--- src/client/src/Components/Common/Link/index.tsx
+++ src/client/src/Components/Common/Link/index.tsx
@@ -2,6 +2,14 @@ import { NavLink, Link } from 'react-router-dom';
 
 import { LinkProps } from './helpers';
 
+export const lightHoverProps = 'hover:text-primary';
+export const darkHoverProps = 'dark:hover:text-primary-dark';
+const hoverProps = `${lightHoverProps} ${darkHoverProps}`;
+
+export const lightActiveProps = 'text-primary';
+export const darkActiveProps = 'dark:text-primary-dark';
+const activeProps = `${lightActiveProps} ${darkActiveProps}`;
+
 export default function CustomLink({
   children,
   to = '/',
@@ -9,18 +17,22 @@ export default function CustomLink({
   className,
   isActive
 }: LinkProps) {
-  const activeProps = isActive && 'text-primary dark:text-primary-dark';
-  const hoverProps = 'hover:text-primary dark:hover:text-primary-dark';
-
   return type === 'navlink' ? (
     <NavLink
       to={to}
-      className={`text-xl py-4 px-2 font-bold ${hoverProps} ${activeProps} ${className}`}
+      data-testid="navlink"
+      className={`text-xl py-4 px-2 font-bold ${hoverProps} ${
+        isActive && activeProps
+      } ${className}`}
     >
       {children}
     </NavLink>
   ) : (
-    <Link to={to} className={`text-lg ${hoverProps} ${className}`}>
+    <Link
+      to={to}
+      data-testid="link"
+      className={`text-lg ${hoverProps} ${className}`}
+    >
       {children}
     </Link>
   );
