f6d613ef4fd7eb8e20b7244908ef65076579d289
Daniel Spasov <shadygotrabies@gmail.com>
2024-02-11 08:53:02 +0200

    Added cursor-pointer on SVG hover

--- src/client/src/Components/Common/Icon/SVG.tsx
+++ src/client/src/Components/Common/Icon/SVG.tsx
@@ -5,7 +5,7 @@ export const darkProps = 'dark:[&>path]:text-neutral-50';
 
 export const lightHoverProps = '[&>path]:hover:text-secondary';
 export const darkHoverProps = 'dark:[&>path]:hover:text-primary-dark';
-const hoverProps = `${lightHoverProps} ${darkHoverProps}`;
+const hoverProps = `cursor-pointer ${lightHoverProps} ${darkHoverProps}`;
 
 export const disabledProps = `[&>path]:text-neutral-300 dark:[&>path]:text-neutral-500`;
 
