fdd7d8859c237c49066105e85c8384e63faed274
Daniel Spasov <shadygotrabies@gmail.com>
2024-03-10 16:29:32 +0200

    Deleted obsolete component

--- src/client/src/Components/Common/index.tsx
+++ src/client/src/Components/Common/index.tsx
@@ -1,7 +1,6 @@
 export { default as Summary } from './Summary';
 export { default as Popover } from './Popover';
 export { default as Tooltip } from './Tooltip';
-export { default as Search } from './Search';
 export { default as Card } from './Card';
 export { default as List } from './List';
 export { default as Icon } from './Icon';
