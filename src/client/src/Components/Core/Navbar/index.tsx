b227b7777e330f5a908a18de9cbbbc5d5d3a80ac
Daniel Spasov <shadygotrabies@gmail.com>
2024-03-11 09:24:46 +0200

    Fixed navbar bottom border in light mode

--- src/client/src/Components/Core/Navbar/index.tsx
+++ src/client/src/Components/Core/Navbar/index.tsx
@@ -4,8 +4,9 @@ import { useCallback, useContext, useState } from 'react';
 import { ThemeContext, AuthContext, Theme } from '../../../Contexts';
 import { Link, Popover, Icon } from '../../';
 
-const lightProps = 'bg-neutral-50';
-const darkProps = 'dark:bg-neutral-950';
+const lightProps = 'bg-neutral-50 border-b-[1px]';
+const darkProps =
+  'dark:bg-neutral-950 dark:border-none dark:shadow-sm dark:shadow-neutral-950';
 const themeProps = `${lightProps} ${darkProps}`;
 
 const userMenuDarkProps = 'dark:border-neutral-900 dark:border-0';
@@ -37,9 +38,7 @@ export default function Navbar() {
   }, []);
 
   return (
-    <nav
-      className={`fixed w-full h-20 z-50 flex px-10 border-b-[1px] ${themeProps}`}
-    >
+    <nav className={`fixed w-full h-20 z-50 flex px-10 ${themeProps}`}>
       <div className="flex items-center flex-1 p-4">
         <Link to="/">
           <img
