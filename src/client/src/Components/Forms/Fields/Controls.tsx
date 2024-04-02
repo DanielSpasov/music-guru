18a98732522cc45b5c0c8f9eca91d1710c93a341
Daniel Spasov <danielspasov4@gmail.com>
2024-04-02 21:52:40 +0300

    Extend the hitbox of the clear button

--- src/client/src/Components/Forms/Fields/Controls.tsx
+++ src/client/src/Components/Forms/Fields/Controls.tsx
@@ -15,11 +15,11 @@ export default function Controls({
 }: ControlsProps) {
   return (
     <div className="absolute top-0 right-0 p-1 flex items-center">
-      <div className="p-2">
+      <div className="p-2 cursor-pointer" onClick={onClear}>
         {value && <Icon model="close" className="w-5 h-5" onClick={onClear} />}
       </div>
       <span className="bg-neutral-200 dark:bg-neutral-600 my-2 w-px self-stretch" />
-      <div className="p-2">
+      <div className="p-2 cursor-pointer" onClick={onClick}>
         <Icon model={iconModel} className="w-5 h-5" onClick={onClick} />
       </div>
     </div>
