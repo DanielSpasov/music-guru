e481361ca17186ae1b2afd6a7f61540c7086860f
Daniel Spasov <danielspasov4@gmail.com>
2024-04-06 15:08:36 +0300

    Fix double onClick & bugged onHover effects on field Controls

--- src/client/src/Components/Forms/Fields/Controls.tsx
+++ src/client/src/Components/Forms/Fields/Controls.tsx
@@ -7,6 +7,10 @@ type ControlsProps = {
   value: any;
 };
 
+const iconLightHoverProps = `[&>svg>path]:hover:fill-secondary`;
+const iconDarkHoverProps = `dark:[&>svg>path]:hover:fill-primary-dark`;
+const iconHoverProps = `${iconLightHoverProps} ${iconDarkHoverProps}`;
+
 export default function Controls({
   iconModel,
   onClick,
@@ -15,12 +19,12 @@ export default function Controls({
 }: ControlsProps) {
   return (
     <div className="absolute top-0 right-0 p-1 flex items-center">
-      <div className="p-2 cursor-pointer" onClick={onClear}>
-        {value && <Icon model="close" className="w-5 h-5" onClick={onClear} />}
+      <div className={`p-2 cursor-pointer ${iconHoverProps}`} onClick={onClear}>
+        {value && <Icon model="close" className="w-5 h-5" />}
       </div>
       <span className="bg-neutral-200 dark:bg-neutral-600 my-2 w-px self-stretch" />
-      <div className="p-2 cursor-pointer" onClick={onClick}>
-        <Icon model={iconModel} className="w-5 h-5" onClick={onClick} />
+      <div className={`p-2 cursor-pointer ${iconHoverProps}`} onClick={onClick}>
+        <Icon model={iconModel} className="w-5 h-5" />
       </div>
     </div>
   );
