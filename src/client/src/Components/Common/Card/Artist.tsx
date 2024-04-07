c64becfdc9618343b0b84bd11b385c1883f062ec
Daniel Spasov <shadygotrabies@gmail.com>
2024-04-07 17:42:34 +0200

    Downsized Artist card to w/h-44

--- src/client/src/Components/Common/Card/Artist.tsx
+++ src/client/src/Components/Common/Card/Artist.tsx
@@ -18,7 +18,7 @@ export default function ArtistCard({
       className={`flex flex-col m-3 cursor-pointer p-2 rounded-md ${hoverProps}`}
       onClick={onClick}
     >
-      <div className="h-52 w-52">
+      <div className="h-44 w-44">
         <img
           alt={data.name}
           src={data.image}
@@ -53,7 +53,7 @@ function Skeleton() {
       data-testid="artist-card-skeleton"
       className="relative bg-neutral-200 dark:bg-neutral-700 flex flex-col m-3 cursor-pointer p-2 rounded-md"
     >
-      <div className="bg-neutral-300 dark:bg-neutral-800 w-52 h-52 rounded-md" />
+      <div className="bg-neutral-300 dark:bg-neutral-800 w-44 h-44 rounded-md" />
       <div className="flex justify-between">
         <div className="bg-neutral-300 dark:bg-neutral-800 w-28 h-6 mt-2 rounded-md" />
         <div className="bg-neutral-300 dark:bg-neutral-800 w-10 h-6 mt-2 rounded-md" />
