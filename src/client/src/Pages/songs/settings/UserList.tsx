27471100e05daaf3b586f2b3e16b401ac2bf3ea5
Daniel Spasov <shadygotrabies@gmail.com>
2024-05-14 09:24:36 +0300

    Added search in userlist

--- src/client/src/Pages/songs/settings/UserList.tsx
+++ src/client/src/Pages/songs/settings/UserList.tsx
@@ -1,5 +1,13 @@
+import { useMemo, useState } from 'react';
+
 import { Icon, Loader } from '../../../Components';
 import { UserListProps } from './helpers';
+
+const lightInputProps =
+  'border-b-neutral-300 hover:border-b-neutral-400 focus:border-b-primary';
+const darkInputProps =
+  'dark:border-b-neutral-800 dark:hover:border-b-neutral-600 dark:focus:border-b-primary-dark';
+const themeInputProps = `${lightInputProps} ${darkInputProps}`;
 
 export default function UserList({
   items,
@@ -8,11 +16,31 @@ export default function UserList({
   missingMessage,
   icon
 }: UserListProps) {
+  const [search, setSearch] = useState('');
+
+  const displayedItems = useMemo(() => {
+    if (!search) return items;
+    return items.filter(user =>
+      user.username.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [items, search]);
+
   return (
     <article
       className={`bg-neutral-200 dark:bg-neutral-950 w-full rounded-md p-2 mt-2`}
     >
-      {items.length ? (
+      <div className="relative">
+        <input
+          onChange={e => setSearch(e.target.value)}
+          className={`bg-transparent border-b-2 outline-none w-full p-1 mb-2 text-lg ${themeInputProps}`}
+          placeholder="Search..."
+          type="text"
+        />
+
+        <Icon model="search" className="absolute top-0 right-0 w-8 h-8" />
+      </div>
+
+      {displayedItems.length ? (
         <>
           <section className="flex px-2">
             <p className="text-lg font-bold w-1/3">Username</p>
@@ -21,7 +49,7 @@ export default function UserList({
 
           <section>
             {!loading ? (
-              items.map(user => (
+              displayedItems.map(user => (
                 <div
                   key={user.uid}
                   className="flex justify-between p-2 rounded-md hover:bg-neutral-300 dark:hover:bg-neutral-800 cursor-pointer"
@@ -38,7 +66,9 @@ export default function UserList({
           </section>
         </>
       ) : (
-        <p className="text-center">{missingMessage}</p>
+        <p className="text-center">
+          {search ? `No results for "${search}"` : missingMessage}
+        </p>
       )}
     </article>
   );
