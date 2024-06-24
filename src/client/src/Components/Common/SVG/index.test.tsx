58987994c8e59526175b0faba76a535ac5be235c
Daniel Spasov <shadygotrabies@gmail.com>
2024-06-24 15:54:02 +0300

    SVG Component tests improvements

--- src/client/src/Components/Common/SVG/index.test.tsx
+++ src/client/src/Components/Common/SVG/index.test.tsx
@@ -10,91 +10,82 @@ import {
 import SVG from './index';
 
 describe('SVG component', () => {
-  describe('Basic props', () => {
-    it('renders SVG without crashing', () => {
+  describe('Rendering', () => {
+    test('renders SVG without crashing', () => {
       render(<SVG />);
       const svgElement = screen.getByTestId('svg-icon');
       expect(svgElement).toBeInTheDocument();
     });
 
-    it('renders SVG with custom class', () => {
+    test('renders correct children', () => {
+      render(
+        <SVG>
+          <path d="M10 10" />
+        </SVG>
+      );
+      const svgElement = screen.getByTestId('svg-icon');
+      expect(svgElement).toContainHTML('<path d="M10 10" />');
+    });
+  });
+
+  describe('Component props', () => {
+    test('renders SVG with custom class', () => {
       const className = 'bg-neutral-50';
       render(<SVG className={className} />);
       const svgElement = screen.getByTestId('svg-icon');
       expect(svgElement).toHaveClass(className);
     });
 
-    it('renders SVG with custom viewBox', () => {
+    test('renders SVG with custom viewBox', () => {
       const viewBox = '0 0 24 24';
       render(<SVG viewBox={viewBox} />);
       const svgElement = screen.getByTestId('svg-icon');
       expect(svgElement).toHaveAttribute('viewBox', viewBox);
     });
 
-    it('renders SVG with children', () => {
-      const children = <path d="M10 10" />;
-      render(<SVG>{children}</SVG>);
-      const svgElement = screen.getByTestId('svg-icon');
-      expect(svgElement).toContainHTML('<path d="M10 10" />');
-    });
-  });
-
-  describe('Bussiness logic', () => {
-    it('calls onClick function when SVG element is clicked', () => {
-      let clickCounter = 0;
-      const onClickMock = () => {
-        clickCounter++;
-      };
-
-      render(<SVG onClick={onClickMock} />);
-
+    test('calls onClick function when SVG element is clicked', () => {
+      const onClick = vi.fn();
+      render(<SVG onClick={onClick} />);
       const svgElement = screen.getByTestId('svg-icon');
       fireEvent.click(svgElement);
-
-      expect(clickCounter).toBe(1);
+      expect(onClick).toBeCalled();
     });
 
-    it("doesn't call onClick function when SVG element is disabled and clicked", () => {
-      let clickCounter = 0;
-      const onClickMock = () => {
-        clickCounter++;
-      };
-
-      render(<SVG onClick={onClickMock} disabled />);
-
+    test("doesn't call onClick function when SVG element is disabled and clicked", () => {
+      const onClick = vi.fn();
+      render(<SVG onClick={onClick} disabled />);
       const svgElement = screen.getByTestId('svg-icon');
       fireEvent.click(svgElement);
-
-      expect(clickCounter).toBe(0);
+      expect(onClick).not.toBeCalled();
     });
   });
 
   describe('CSS', () => {
-    it('renders dark mode', () => {
+    test('renders dark mode', () => {
       render(<SVG />);
       const svgElement = screen.getByTestId('svg-icon');
       expect(svgElement).toHaveClass(darkProps);
     });
 
-    it('renders light mode', () => {
+    test('renders light mode', () => {
       render(<SVG />);
       const svgElement = screen.getByTestId('svg-icon');
       expect(svgElement).toHaveClass(lightProps);
     });
 
-    it('renders dark mode hover', () => {
+    test('renders dark mode hover', () => {
       render(<SVG onClick={() => null} />);
       const svgElement = screen.getByTestId('svg-icon');
       expect(svgElement).toHaveClass(darkHoverProps);
     });
 
-    it('renders light mode hover', () => {
+    test('renders light mode hover', () => {
       render(<SVG onClick={() => null} />);
       const svgElement = screen.getByTestId('svg-icon');
       expect(svgElement).toHaveClass(lightHoverProps);
     });
 
-    it('renders disabled mode', () => {
+    test('renders disabled mode', () => {
       render(<SVG onClick={() => null} disabled />);
       const svgElement = screen.getByTestId('svg-icon');
       expect(svgElement).toHaveClass(disabledProps);
