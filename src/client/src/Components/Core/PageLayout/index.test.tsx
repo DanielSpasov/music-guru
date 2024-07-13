770df5c7605d8ea323dd37020542cc1c29e58a6f
Daniel Spasov <shadygotrabies@gmail.com>
2024-07-13 09:10:00 +0300

    Updated PageLayout tests with recently viewed changes

--- src/client/src/Components/Core/PageLayout/index.test.tsx
+++ src/client/src/Components/Core/PageLayout/index.test.tsx
@@ -1,9 +1,31 @@
 import { screen, render } from '@testing-library/react';
-import PageLayout from '.';
 import { MemoryRouter } from 'react-router-dom';
 
+import PageLayout from '.';
+
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useLocation: vi.fn().mockReturnValue({ pathname: '/test/pathname' })
+  };
+});
+
+const addCurrentMock = vi.fn();
+
+vi.mock('../../../Hooks/useRecentlyViewed', () => ({
+  default: () => ({
+    addCurrent: addCurrentMock,
+    recentlyViewed: []
+  })
+}));
+
 describe('PageLayout', () => {
-  test('renders without crashing', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test('renders without crashing', async () => {
     render(
       <MemoryRouter>
         <PageLayout title="Test">
@@ -37,6 +59,8 @@ describe('PageLayout', () => {
 
     const loaderEl = screen.queryByTestId('page-loader');
     expect(loaderEl).not.toBeInTheDocument();
+
+    expect(addCurrentMock).toHaveBeenCalled();
   });
 
   test('renders with additional props', () => {
@@ -68,5 +92,46 @@ describe('PageLayout', () => {
 
     const loaderEl = screen.queryByTestId('page-loader');
     expect(loaderEl).toBeInTheDocument();
+
+    expect(addCurrentMock).not.toHaveBeenCalled();
+  });
+
+  test("doesn't save recent if dontSaveRecent is true", () => {
+    render(
+      <MemoryRouter>
+        <PageLayout
+          title="Test"
+          hideNavbar
+          hideSidebar
+          hideHeader
+          dontSaveRecent
+        />
+      </MemoryRouter>
+    );
+
+    const pageEl = screen.getByTestId('page');
+    expect(pageEl).toBeInTheDocument();
+
+    const pageBodyEl = screen.getByTestId('page-body');
+    expect(pageBodyEl).toBeInTheDocument();
+    expect(pageBodyEl).not.toHaveClass('pt-16');
+
+    const navbarEl = screen.queryByTestId('navbar');
+    expect(navbarEl).not.toBeInTheDocument();
+
+    const pageContentEl = screen.getByTestId('page-content');
+    expect(pageContentEl).toBeInTheDocument();
+    expect(pageContentEl).not.toHaveClass('ml-64');
+
+    const sidebarEl = screen.queryByTestId('sidebar');
+    expect(sidebarEl).not.toBeInTheDocument();
+
+    const headerEl = screen.queryByTestId('header');
+    expect(headerEl).not.toBeInTheDocument();
+
+    const loaderEl = screen.queryByTestId('page-loader');
+    expect(loaderEl).not.toBeInTheDocument();
+
+    expect(addCurrentMock).not.toHaveBeenCalled();
   });
 });
