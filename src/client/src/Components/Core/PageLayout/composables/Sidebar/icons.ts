fcaedb18d78c361d1fdfe7bb90ee3e20eeba0545
Daniel Spasov <shadygotrabies@gmail.com>
2024-08-06 07:30:23 +0300

    Fix sidebar icon colors

--- src/client/src/Components/Core/PageLayout/composables/Sidebar/icons.ts
+++ src/client/src/Components/Core/PageLayout/composables/Sidebar/icons.ts
@@ -10,11 +10,16 @@ export const icons: Record<string, ElementType> = {
 };
 
 export const iconColor: Record<string, string> = {
-  artists: '[&>path]:fill-black dark:[&>path]:fill-white'
+  artists: '[&>path]:fill-black dark:[&>path]:fill-white',
+  settings: '[&>path]:fill-black dark:[&>path]:fill-white',
+  'access-denied':
+    '[&>path:nth-child(2)]:fill-black dark:[&>path:nth-child(2)]:fill-white'
 };
 
 export const activeIconColor: Record<string, string> = {
   artists: '[&>path]:fill-primary dark:[&>path]:fill-primary-dark',
   albums: '[&>g]:stroke-primary dark:[&>g]:stroke-primary-dark',
-  songs: '[&>g]:stroke-primary dark:[&>g]:stroke-primary-dark'
+  songs: '[&>g]:stroke-primary dark:[&>g]:stroke-primary-dark',
+  'access-denied':
+    '[&>path:nth-child(2)]:fill-primary dark:[&>path:nth-child(2)]:fill-primary-dark'
 };
