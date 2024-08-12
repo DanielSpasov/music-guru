8638819e8204809fd3291d396b3085a21a9a135b
Daniel Spasov <shadygotrabies@gmail.com>
2024-08-12 13:23:09 +0300

    Fix bg-colors in PageLayout

--- src/client/src/Components/Core/PageLayout/composables/Footer/index.tsx
+++ src/client/src/Components/Core/PageLayout/composables/Footer/index.tsx
@@ -4,7 +4,7 @@ import { FooterProps } from './types';
 
 const Footer: FC<FooterProps> = ({ children }) => {
   return (
-    <footer className="bg-neutral-900 border-t-[1px] border-t-neutral-700 p-4">
+    <footer className="bg-neutral-100 dark:bg-neutral-900 border-t-[1px] border-t-neutral-200 dark:border-t-neutral-700 p-4">
       {children}
     </footer>
   );
