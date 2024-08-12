2007d3f40b1161ae0b5d373a352d425c162e3c98
Daniel Spasov <shadygotrabies@gmail.com>
2024-08-12 13:56:55 +0300

    Add Footer tests

--- src/client/src/Components/Core/PageLayout/composables/Footer/index.tsx
+++ src/client/src/Components/Core/PageLayout/composables/Footer/index.tsx
@@ -4,7 +4,10 @@ import { FooterProps } from './types';
 
 const Footer: FC<FooterProps> = ({ children }) => {
   return (
-    <footer className="bg-neutral-100 dark:bg-neutral-900 border-t-[1px] border-t-neutral-200 dark:border-t-neutral-700 p-4">
+    <footer
+      data-testid="footer"
+      className="bg-neutral-100 dark:bg-neutral-900 border-t-[1px] border-t-neutral-200 dark:border-t-neutral-700 p-4"
+    >
       {children}
     </footer>
   );
