f20a4214f37fbc16b1a5274528fdc0fdc3021ae4
Daniel Spasov <shadygotrabies@gmail.com>
2024-08-27 21:55:29 +0300

    Responsive design improvements in song details

--- packages/client/src/Pages/songs/details/composables/Item/composables/MissingText/index.tsx
+++ packages/client/src/Pages/songs/details/composables/Item/composables/MissingText/index.tsx
@@ -3,7 +3,11 @@ import { FC, memo } from 'react';
 import { MissingTextProps } from './types';
 
 const MissingText: FC<MissingTextProps> = ({ message = 'None' }) => {
-  return <span className="text-lg text-neutral-400">{message}</span>;
+  return (
+    <span className="text-lg text-neutral-400 whitespace-nowrap">
+      {message}
+    </span>
+  );
 };
 
 export default memo(MissingText);
